46a606bda8367738fc581a11e76e6268481fc495
Alain ANDRE <wordsbybird@alain-andre.fr>
2021-04-21 17:03:18 +0200

    CI: add content-type to curl requests

--- .github/actions/deliver.sh
+++ .github/actions/deliver.sh
@@ -4,7 +4,7 @@ export GEM_VERSION=${GITHUB_REF#refs/*/v}
 echo "Building gem ${GEM_VERSION}"
 gem build mapotempo_rubocop.gemspec
 
-status_code=$(curl  -s -o /dev/null -w "%{http_code}" --data-binary mapotempo_rubocop-${GEM_VERSION}.gem -H "Authorization:${RUBYGEM_API}" 'https://rubygems.org/api/v1/gems')
+status_code=$(curl -s -o /dev/null -w "%{http_code}" --data-binary @mapotempo_rubocop-${GEM_VERSION}.gem -H "Authorization:${RUBYGEM_API}" -H "Content-Type: application/octet-stream" 'https://rubygems.org/api/v1/gems')
 
 if [ "${status_code}" != "200" ]; then
   echo "Error delivering the gem to rubygems.org (status ${status_code})."
